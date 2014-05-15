72ad84a60b497623bafbe5779d0f21b3d1a201ff
igorTavares <igorborgest@gmail.com>
2014-05-15 06:45:25 +0000

    Average added in test routine.

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
 # neon_test
+
 This program tests the ARM NEON™ instruction set, with a simple and comparative
-routine.
+test routine.
 
-The routine is to divide by two all the elements of two arrays, then add all
-the elements of the same index of these two arrays, and then the result is
-incremented in 127 and stored in a third array.
-
-Each test run 300 times the test routine and measure the elapsed time.
+The test routine averages each index of the input arrays and store the result in a
+third array.
 
 ### To build and test
 
--- main.c
+++ main.c
@@ -1,24 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include <arm_neon.h>
 
-#define SIZE 1600000 // Arrays size
+#define SIZE 64000000 // Arrays size
 
-void routine(uint8_t *arr2, uint8_t *arr1, uint8_t *arr0, uint8_t offset);
-void neon_routine(uint8_t *arr2, uint8_t *arr1, uint8_t *arr0, uint8_t offset);
+void routine(uint8_t *arr2, uint8_t *arr1, uint8_t *arr0);
+void neon_routine(uint8_t *arr2, uint8_t *arr1, uint8_t *arr0);
 
 int main(void){
   uint32_t i;
-  uint8_t arr0[SIZE];
-  uint8_t arr1[SIZE];
-  uint8_t arr2[SIZE];
+  uint8_t *arr0;
+  uint8_t *arr1;
+  uint8_t *arr2;
+
+  arr0 = (uint8_t *)malloc(SIZE);
+  arr1 = (uint8_t *)malloc(SIZE);
+  arr2 = (uint8_t *)malloc(SIZE);
 
   struct timespec start, end;
   uint32_t diff;
 
   printf("\nStarting the NEON test...\n");
 
-  for(i=0; i<SIZE; i++){
+  for(i=0; i<SIZE; i++){ // Populates the input arrays
     arr0[i] = i;
     arr1[i] = i;
   }
@@ -26,9 +31,7 @@ int main(void){
   printf("Starting the test routine w/o NEON.\n");
   clock_gettime( CLOCK_REALTIME, &start );
 
-  for(i=0; i<300; i++){
-    routine(arr2, arr1, arr0, 127);
-  }
+  routine(arr2, arr1, arr0);
 
   clock_gettime( CLOCK_REALTIME, &end );
   diff = (end.tv_sec-start.tv_sec)*1000 + (end.tv_nsec-start.tv_nsec)/1000000;
@@ -37,9 +40,7 @@ int main(void){
   printf("Starting the test routine with NEON.\n");
   clock_gettime( CLOCK_REALTIME, &start );
 
-  for(i=0; i<300; i++){
-    neon_routine(arr2, arr1, arr0, 127);
-  }
+  neon_routine(arr2, arr1, arr0);
 
   clock_gettime( CLOCK_REALTIME, &end );
   diff = (end.tv_sec-start.tv_sec)*1000 + (end.tv_nsec-start.tv_nsec)/1000000;
@@ -48,17 +49,15 @@ int main(void){
   return 0;
 }
 
-void neon_routine(uint8_t *arr2, uint8_t *arr1, uint8_t *arr0, uint8_t offset){
+void neon_routine(uint8_t *arr2, uint8_t *arr1, uint8_t *arr0){
 
   uint32_t i;
   uint8x16_t arr0_;
   uint8x16_t arr1_;
 
-  uint8x16_t arr3_ = vdupq_n_u8(offset);
-
   for(i=0; i<SIZE; i+=16){
     arr0_ = vld1q_u8(arr0 + i); // arr0_ = arr0
-    arr0_ = vsraq_n_u8(arr3_, arr0_, 1); // arr0_ = arr3 + (arr0_ >> 1)
+    arr0_ = vshrq_n_u8(arr0_, 1); // arr0_ = arr0_ >> 1
 
     arr1_ = vld1q_u8(arr1 + i); // arr1_ = arr1
     arr0_ = vsraq_n_u8(arr0_, arr1_, 1); // arr0_ = arr0_ + (arr1_ >> 1)
@@ -67,15 +66,11 @@ void neon_routine(uint8_t *arr2, uint8_t *arr1, uint8_t *arr0, uint8_t offset){
   }
 }
 
-void routine(uint8_t *arr2, uint8_t *arr1, uint8_t *arr0, uint8_t offset){
+void routine(uint8_t *arr2, uint8_t *arr1, uint8_t *arr0){
 
   uint32_t i;
-  uint8_t tmp0, tmp1;
 
   for(i=0; i<SIZE; i++){
-    tmp0 = arr0[i] >> 1;
-    tmp1 = arr1[i] >> 1;
-    arr2[i] = tmp0 + tmp1;
-    arr2[i] += offset;
+    arr2[i] = (arr0[i] >> 1) + (arr1[i] >> 1);
   }
 }
